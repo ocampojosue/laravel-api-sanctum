a6f9a1d75490956f2ea66657fb1a270158808859
ocampojosue <ocampojosue08@gmail.com>
2022-06-27 17:38:32 -0400

    Migration

---
+++ database/migrations/2022_06_27_212902_create_patients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('age');
+            $table->string('sex');
+            $table->string('ci', 10)->unique();
+            $table->string('type_blood');
+            $table->string('phone', 8);
+            $table->string('email');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
